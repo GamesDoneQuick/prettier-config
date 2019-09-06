b8703196aba47cb6cb16941e99dcded767e581b9
Alex Van Camp <vancamp.alex@gmail.com>
2019-09-05 20:08:33 -0500

    feat: use singleQuote (#1)
    BREAKING CHANGE: Single quotes are now preferred over double quotes.

--- prettier.config.js
+++ prettier.config.js
@@ -4,4 +4,9 @@ module.exports = {
   /* This can help reduce diff sizes, so we generally like it
    * as a best practice to make code reviews just that tiny bit better. */
   trailingComma: "all",
+  
+  /* A single quote requires pressing only one key,
+   * but a double quote requires holding shift.
+   * So, we find single quotes easier to type and prefer them. */
+  singleQuote: true
 };
