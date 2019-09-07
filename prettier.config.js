8398453176ce283af385559b5d3373ebd06c911b
Alex Van Camp <vancamp.alex@gmail.com>
2019-09-07 10:45:18 -0500

    feat(printWidth): set to 120 columns
    I'm open to debate on this. I'm not super hard sold on this or anything, just thought it'd be good to bring up and get everyone's thoughts on.

--- prettier.config.js
+++ prettier.config.js
@@ -8,5 +8,18 @@ module.exports = {
   /* A single quote requires pressing only one key,
    * but a double quote requires holding shift.
    * So, we find single quotes easier to type and prefer them. */
-  singleQuote: true
+  singleQuote: true,
+  
+  /* It's true that Prettier warns pretty heavily against setting
+   * this higher than 80 columns: https://prettier.io/docs/en/options.html#print-width
+   *
+   * However, Prettier itself already disregards this warning in
+   * the name of readability: https://prettier.io/docs/en/rationale.html#imports
+   *
+   * Our friend Faulty has been using 120 for some time, and has had no issues.
+   * Indeed, all of Discord seems to use this setting.
+   * 
+   * Lange: having tested this on some of my own real-world code,
+   * I agree with Faulty that it does make things easier to read.
+   printWidth: 120,
 };
